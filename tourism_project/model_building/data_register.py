6ebec2016009300ca6769db6d3d1472906d6f525
TharunNo1 <tharunganapathik@gmail.com>
2026-07-17 05:32:12 +0000

    HF login exception handling added

--- tourism_project/model_building/data_register.py
+++ tourism_project/model_building/data_register.py
@@ -6,11 +6,18 @@ import os
 repo_id = "tharung492/tourism-purchase-prediction"
 repo_type = "dataset"
 
+hf_token = os.getenv("HF_TOKEN")
+if not hf_token:
+    raise ValueError(
+        "❌ Critical Error: 'HF_TOKEN' environment variable is not set! "
+        "Make sure you passed it in your GitHub Actions YAML file under the 'env:' block."
+    )
+
 # Authenticate using the token
-login(token=os.getenv("HF_TOKEN"))
+login(token=hf_token)
 
 # Initialize API client
-api = HfApi(token=os.getenv("HF_TOKEN"))
+api = HfApi(token=hf_token)
 
 # Step 1: Check if the space exists
 try:
